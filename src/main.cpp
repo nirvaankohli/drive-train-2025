9463c81c580d5de0472c2d25d0df42668d637474
Nirvaan Kohli <picklewagonwmhs@gmail.com>
2025-10-21 15:42:11 -0400

    Init 10/21/2025 w other coders
    Co-authored-by: KuroKame <KuroKamee@users.noreply.github.com>
    Co-authored-by: Ghandi67 <Ghandi67@users.noreply.github.com>

--- src/main.cpp
+++ src/main.cpp
@@ -11,14 +11,13 @@ ez::Drive chassis(
      // These are your drive motors, the first motor is used for sensing!
     {-12, -13, -14},     // Left Chassis Ports (negative port will reverse it!)
     {1, 2, 3},  // Right Chassis Ports (negative port will reverse it!)
-
     4,      // IMU Port
     2.25,  // Wheel Diameter (Remember, 4" wheels without screw holes are actually 4.125!)
     343);   // Wheel RPM = cartridge * (motor gear / wheel gear)
-// Uncomment the trackers you're using here!
+// Uncomment the trackers you're using here!.
 // - `8` and `9` are smart ports (making these negative will reverse the sensor)
-//  - you should get positive values on the encoders going FORWARD and RIGHT
-// - `2.75` is the wheel diameter
+//  - you should get positive values on the encoders going FORWARD and 
+// - `2.75` is the wheel diameter 
 // - `4.0` is the distance from the center of the wheel to the center of the robot
 // ez::tracking_wheel horiz_tracker(8, 2.75, 4.0);  // This tracking wheel is perpendicular to the drive wheels
 // ez::tracking_wheel vert_tracker(9, 2.75, 4.0);   // This tracking wheel is parallel to the drive wheels
