650abf94f89965dcee2512e52ec828a9c87ab424
Nirvaan Kohli <picklewagonwmhs@gmail.com>
2025-10-23 16:40:38 -0400

    soeanr qoekk

---
+++ ghandi67.cpp
@@ -0,0 +1,11 @@
+void ghandhi67HaveMyKid() {
+
+  for (int i = 0; i < 67; i++) {
+
+    cout << "Oops, another one popped out! This is number " << i << " with Kurokame";
+    
+  }
+  
+}
+
+ghandhi67HaveMyKid();
--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,8 @@
 /////
 // 35.25
 // Chassis constructor
+
+
 ez::Drive chassis(
      // These are your drive motors, the first motor is used for sensing!
     {-12, -13, -14},     // Left Chassis Ports (negative port will reverse it!)
@@ -20,10 +22,55 @@ ez::Drive chassis(
     (600 * (36 / 60)));   // Wheel RPM = cartridge * (motor gear / wheel gear)
 
 
+ez::tracking_wheel horiz_tracker(5, 2, 4.0);  // This tracking wheel is perpendicular to the drive wheels
+ez::tracking_wheel vert_tracker(8, 2, 6.125);
 
-// 8 is X, 5 is Y
+void odom_test_routine();
+
+void odom_test_routine() {
+
+    chassis.pid_targets_reset();
+    chassis.drive_imu_reset();
+    chassis.drive_sensor_reset();
+
+    chassis.drive_brake_set(MOTOR_BRAKE_HOLD);
+
+    chassis.odom_xyt_set(0_in, 0_in, 0_deg);
+
+    chassis.pid_odom_set({{24_in, 24_in}, fwd, 110, ez::cw});
+    chassis.pid_wait();
+    
+    
+    
+}
+
+// pros::Motor front_right_motor(3, pros::v5::MotorGears::red);
+// pros::Motor middle_right_motor(1, pros::v5::MotorGears::red);
+// pros::Motor back_right_motor(2, pros::v5::MotorGears::red);
+// pros::Motor front_left_motor(-13, pros::v5::MotorGears::red);
+// pros::Motor middle_left_motor(-12, pros::v5::MotorGears::red);
+// pros::Motor back_left_motor(-14, pros::v5::MotorGears::red);
+
+// pros::MotorGroup right_motor_group({ 3, 1, 2 });
+// pros::MotorGroup left_motor_group({ -13, -12, -14 });
 
 
+// // 8 is X, 5 is Y
+// // 35.2 track width
+
+// lemlib::Drivetrain drivetrain(&left_motor_group, // left motor group
+//                               &right_motor_group, // right motor group
+//                               13.6, 
+//                               lemlib::Omniwheel::NEW_325,
+//                               360, // drivetrain rpm is 360
+//                               2);
+
+// pros::Imu imu(4);
+
+// pros::Rotation horizontal_rotation(8);
+// pros::Rotation vertical_rotation(5);
+// \    
+ 
 
 // Uncomment the trackers you're using here!.
 // - `8` and `9` are smart ports (making these negative will reverse the sensor)
@@ -40,64 +87,39 @@ ez::Drive chassis(
  * to keep execution time for this mode under a few seconds.
  */
 
-void ghandhi67HaveMyKid() {
 
-  for (int i = 0; i < 67; i++) {
-
-    cout << "Oops, another one popped out! This is number " << i << " with Kurokame"
-    
-  }
-  
-}
 void initialize() {
-  // Print our branding over your terminal :D
+
   ez::ez_template_print();
 
   pros::delay(500);  // Stop the user from doing anything while legacy ports configure
 
-  // Look at your horizontal tracking wheel and decide if it's in front of the midline of your robot or behind it
-  //  - change `back` to `front` if the tracking wheel is in front of the midline
-  //  - ignore this if you aren't using a horizontal tracker
-  // chassis.odom_tracker_back_set(&horiz_tracker);
-  // Look at your vertical tracking wheel and decide if it's to the left or right of the center of the robot
-  //  - change `left` to `right` if the tracking wheel is to the right of the centerline
-  //  - ignore this if you aren't using a vertical tracker
-  // chassis.odom_tracker_left_set(&vert_tracker);
-
-  // Configure your chassis controls
+  chassis.odom_tracker_back_set(&horiz_tracker);
+  chassis.odom_tracker_left_set(&vert_tracker);
   chassis.opcontrol_curve_buttons_toggle(true);   // Enables modifying the controller curve with buttons on the joysticks
   chassis.opcontrol_drive_activebrake_set(0.0);   // Sets the active brake kP. We recommend ~2.  0 will disable.
   chassis.opcontrol_curve_default_set(0.0, 0.0);  // Defaults for curve. If using tank, only the first parameter is used. (Comment this line out if you have an SD card!)
 
   // Set the drive to your own constants from autons.cpp!
   default_constants();
+  
 
   // These are already defaulted to these buttons, but you can change the left/right curve buttons here!
   // chassis.opcontrol_curve_buttons_left_set(pros::E_CONTROLLER_DIGITAL_LEFT, pros::E_CONTROLLER_DIGITAL_RIGHT);  // If using tank, only the left side is used.
   // chassis.opcontrol_curve_buttons_right_set(pros::E_CONTROLLER_DIGITAL_Y, pros::E_CONTROLLER_DIGITAL_A);
 
   // Autonomous Selector using LLEMU
-  ez::as::auton_selector.autons_add({
-      {"Drive\n\nDrive forward and come back", drive_example},
-      {"Turn\n\nTurn 3 times.", turn_example},
-      {"Drive and Turn\n\nDrive forward, turn, come back", drive_and_turn},
-      {"Drive and Turn\n\nSlow down during drive", wait_until_change_speed},
-      {"Swing Turn\n\nSwing in an 'S' curve", swing_example},
-      {"Motion Chaining\n\nDrive forward, turn, and come back, but blend everything together :D", motion_chaining},
-      {"Combine all 3 movements", combining_movements},
-      {"Interference\n\nAfter driving forward, robot performs differently if interfered or not", interfered_example},
-      {"Simple Odom\n\nThis is the same as the drive example, but it uses odom instead!", odom_drive_example},
-      {"Pure Pursuit\n\nGo to (0, 30) and pass through (6, 10) on the way.  Come back to (0, 0)", odom_pure_pursuit_example},
-      {"Pure Pursuit Wait Until\n\nGo to (24, 24) but start running an intake once the robot passes (12, 24)", odom_pure_pursuit_wait_until_example},
-      {"Boomerang\n\nGo to (0, 24, 45) then come back to (0, 0, 0)", odom_boomerang_example},
-      {"Boomerang Pure Pursuit\n\nGo to (0, 24, 45) on the way to (24, 24) then come back to (0, 0, 0)", odom_boomerang_injected_pure_pursuit_example},
-      {"Measure Offsets\n\nThis will turn the robot a bunch of times and calculate your offsets for your tracking wheels.", measure_offsets},
-  });
+ 
 
   // Initialize chassis and auton selector
   chassis.initialize();
   ez::as::initialize();
-  master.rumble(chassis.drive_imu_calibrated() ? "." : "---");
+
+  ez::as::auton_selector.autons_add({
+      {"Odom Test\n\nDrive, Turn, Drive, and print odom", odom_test_routine},
+  });
+   
+
 }
 
 /**
